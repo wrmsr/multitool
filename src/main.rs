060a45f1848df14734b8a56b70c7d83e0b2ba42b
wrmsr <timwilloney@gmail.com>
2019-09-28 06:30:41 -0700

    --

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::{App, Arg};
+
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("myprog")
+        .arg(Arg::with_name("eff")
+            .short("f"))
+        .arg(Arg::with_name("pea")
+            .short("p")
+            .takes_value(true))
+        .arg(Arg::with_name("slop")
+            .multiple(true)
+            .last(true))
+        .get_matches();
+
+    println!("-f used: {:?}", matches.is_present("eff"));
+    println!("-p's value: {:?}", matches.value_of("pea"));
+    println!("'slops' values: {:?}", matches.values_of("slop").map(|vals| vals.collect::<Vec<_>>()));
 }
